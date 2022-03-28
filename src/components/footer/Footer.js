a60eba18c49baf0c441f8fa684dee4e200b82262
amsu21 <aswb4@hotmail.com>
2022-03-28 10:49:22 -0400

    source files

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="copyright">
+        Made by Amsu Warner
+        <span role="img" aria-label="thinking">
+        </span>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
