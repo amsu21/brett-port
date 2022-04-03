5554dc31899d5187e599fd23dbd090db29afed51
amsu21 <aswb4@hotmail.com>
2022-04-03 13:03:21 -0400

    Modified Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -29,14 +29,14 @@ const Navbar = () => {
         </div>
         <div className="navlink-wrapper">
           <Link to="experience" spy={true} smooth={true} duration={500}>
-            EXPERIENCE
-          </Link>
-        </div>
-        <div className="navlink-wrapper">
-          <Link to="projects" spy={true} smooth={true} duration={500}>
             SERVICES
           </Link>
         </div>
+        {/* <div className="navlink-wrapper">
+          <Link to="projects" spy={true} smooth={true} duration={500}>
+            SERVICES
+          </Link>
+        </div> */}
         <div className="navlink-wrapper">
           <Link to="contact" spy={true} smooth={true} duration={500}>
             CONTACT
