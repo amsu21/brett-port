430be9212a12ee1bdcfe5838ea82fc67dcde814d
amsu21 <aswb4@hotmail.com>
2022-04-08 10:57:07 -0400

    Removed unnecessary icons

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,9 +3,8 @@ import './Contact.css'
 import Section from '../section/Section'
 import { Bounce } from 'react-reveal'
 import ContactForm from '../contactForm/ContactForm'
-import linkedin from '../../images/social/linkedin.png'
 import github from '../../images/social/github.png'
-import facebook from '../../images/social/facebook.png'
+
 
 const Contact = () => {
   return (
@@ -19,21 +18,6 @@ const Contact = () => {
             rel="noopener noreferrer"
           >
             <img src={github} alt="Github Logo" width="40px" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/mjigalin/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={linkedin} alt="Linkedin Logo" width="40px" />
-          </a>
-
-          <a
-            href="https://www.facebook.com/matthew.jigalin/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={facebook} alt="Facebook Logo" width="40px" />
           </a>
         </div>
       </Bounce>
