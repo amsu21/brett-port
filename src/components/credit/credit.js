d58e85399a008f1e16566a66a5b6e502d16ab15d
amsu21 <aswb4@hotmail.com>
2022-05-05 21:53:44 -0400

    Added fade to card

--- src/components/credit/credit.js
+++ src/components/credit/credit.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './credit.css'
 import { Fade } from 'react-reveal'
 import Section from '../section/Section'
+import { CreditCard } from '@material-ui/icons'
 
 
 const Credit = () => {
@@ -11,10 +12,22 @@ const Credit = () => {
                 <ul className="credit-list">
                     {creditData.credit.reverse().map((exp) => (
                         <Fade bottom duration={1000} distance="20px">
-                            <
+                            <CreditCard credit={exp} />
                         </Fade>
                     ))}
                 </ul>
+                <Fade bottom duration={1200} distance="20px">
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <p style={{ textAlign: 'center' }}>
+                            Blah blah blah
+                        </p>
+                    </div>
+                </Fade>
             </div>
         </Section>
     )
