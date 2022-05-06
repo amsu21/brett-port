c922b0d31c1ed2e6716e519cb59457426fb15a95
amsu21 <aswb4@hotmail.com>
2022-05-05 20:24:28 -0400

    Make new credit folder

---
+++ src/credit/credit.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './credit.css'
+import { Fade } from 'react-reveal'
+import Section from '../section/Section'
+
+
+const Credit = () => {
+    return (
+        <Section title="CREDIT REPARI PROGRAMS">
+            <div className="credit-content">
+                <ul className="credit-list">
+                    
+                </ul>
+            </div>
+        </Section>
+    )
+}
