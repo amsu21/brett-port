ea0be1d3d750e8533e57238ecfa7b98079705ac0
amsu21 <aswb4@hotmail.com>
2022-05-05 21:52:37 -0400

    Added code

---
+++ src/components/creditCard/CreditCard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './CreditCard.css'
+
+const CreditCard = ({ credit }) => {
+    let { link, title, info } = credit 
+    return (
+        <a
+            className="credit-link"
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <div className="credit-card-wrapper">
+                <div className="credit-card">
+                    <div className="credit-card-top">
+                        <div
+                            className="credit-bg"
+                            style={{ background: credit.colourPrimary }}
+                        ></div>
+                        <h2>{company}</h2>
+                        <div className="image-wrapper">
+                            <div
+                                className="credit-bg logo-bg"
+                                style={{
+                                    background: experience.colourSecondary
+                                    ? experience.colourSecondary
+                                    : experience.colourPrimary,
+                                }}
+                            ></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
